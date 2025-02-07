51dd9d1d6f7f7100ba0c753a0f0994ac06dd9a9a
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-02-08 00:50:16 +0500

    loading screen completed but with few biugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./Components/LoadingScreen";
 import "./index.css";
 
 function App() {
+  const [isLoaded, setLoaded] = useState(true);
   return (
     <>
-      <LoadingScreen />
+      {!isLoaded && <LoadingScreen onComplete={() => setLoaded(true)} />}
     </>
   );
 
