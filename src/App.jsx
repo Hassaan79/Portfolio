333596614fcfadb6af2d5968fa0d09a9612451cc
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-02-08 00:24:36 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { LoadingScreen } from "./Components/LoadingScreen";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <LoadingScreen />
+    </>
+  );
+
+}
+
+export default App;
