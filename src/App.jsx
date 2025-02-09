a37a875aa5f00aedce81621b0ef6cb8b26cdb1a7
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-02-09 12:58:31 +0500

    Created Navbar, but its not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./Components/LoadingScreen";
+import { Navbar } from "./Components/Navbar";
 import "./index.css";
 
 function App() {
-  const [isLoaded, setLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setLoaded(true)} />}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
+          } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+
+      </div>
     </>
   );
-
 }
 
 export default App;
